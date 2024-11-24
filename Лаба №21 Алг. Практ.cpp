f4945e992808109cbe931549c238c3f15dd29f33
BlackVarmir <blackvarmir@gmail.com>
2024-11-25 00:08:04 +0200

    Add project files.

---
+++ Лаба №21 Алг. Практ.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    int first, second;
+
+	cout << "P";
+	cin >> first >> second;
+
+	cout << "Press any key to continue . . ." << endl;
+	_getch();
+	system("cls");
+
+	while (true) {
+		int input;
+
+		if (cin >> input) {
+			if (input == first || input == second) {
+				cout << "Press any key to continue . . ." << endl;
+				_getch();
+				system("cls");
+			} else {
+				while (true) {
+					system("cls");
+				}
+			}
+		} else {
+			while (true) {
+				system("cls");
+			}
+		}
+	}
+
+	return 0;
+}
+
+
